66239d40caccc071505943f5f6eeed8e9af056cb
john-g-g <john.gerlock@gmail.com>
2014-04-17 21:46:09 -0400

    Fix bad URL in example

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Run this as a standalone script or wuth curl:
-# bash <(curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/install.sh)
+set -o nounset
+set -o errexit
 
-curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> test
-curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> test
+# Run this as a standalone script or wuth curl:
+# bash <(curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/install.sh)
+
+
+echo 'Adding clang'
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> ~/.ssh/authorized_keys
+
+echo 'Adding k3'
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> ~/.ssh/authorized_keys
