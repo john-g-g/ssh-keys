2f8df868bb49bad4b1331ab609b36fbed1d175b2
john-g-g <john.gerlock@gmail.com>
2014-04-17 22:28:36 -0400

    Add mkdir test

--- install.sh
+++ install.sh
@@ -5,9 +5,13 @@ set -o errexit
 # Run this as a standalone script or wuth curl:
 # bash <(curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/install.sh)
 
+if [[ ! -d ~/.ssh ]]; do
+    mkdir ~/.ssh
+fi    
 
 echo 'Adding clang'
 curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> ~/.ssh/authorized_keys
 
 echo 'Adding k3'
 curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> ~/.ssh/authorized_keys
+
