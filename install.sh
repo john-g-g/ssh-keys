1f0af0869f5fc1c925cfea22be82195a7d32bb4e
john-g-g <john.gerlock@gmail.com>
2014-04-17 21:39:44 -0400

    Test new install method

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Run this as a standalone script or wuth curl:
+# bash <(curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/install.sh)
 
-curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> ~/.ssh/authorized_keys
-curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> ~/.ssh/authorized_keys
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> test
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> test
