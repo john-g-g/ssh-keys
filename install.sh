ff43f81cc674ea5376502ce4487a45477d4047fe
john-g-g <john.gerlock@gmail.com>
2014-04-17 21:31:25 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/clang_id_rsa.pub >> ~/.ssh/authorized_keys
+curl -fsSL https://raw.githubusercontent.com/john-g-g/ssh-keys/master/k3_id_rsa.pub >> ~/.ssh/authorized_keys
